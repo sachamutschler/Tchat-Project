8ec273e6ced609d65eb5365bf53709734c14bac1
Sacha Mutschler <90270486+sachamutschler@users.noreply.github.com>
2022-02-21 12:52:18 +0100

    Add files via upload

---
+++ messenger.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 21 juin 2021 à 22:07
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `messenger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `minichat`
+--
+
+DROP TABLE IF EXISTS `minichat`;
+CREATE TABLE IF NOT EXISTS `minichat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `minichat`
+--
+
+INSERT INTO `minichat` (`id`, `pseudo`, `message`) VALUES
+(9, 'oui', 'comment allez vous ?'),
+(8, 'oui', 'test'),
+(7, 'oui', 'bonjour'),
+(6, 'oui', 'bonjour'),
+(10, 'oui', 'j\'effectue un test'),
+(11, 'oui', 'wow'),
+(12, 'non', 'ça fonctionne !'),
+(13, 'non', 'j\'effectue un test'),
+(14, 'non', 'je fais encore un test');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
